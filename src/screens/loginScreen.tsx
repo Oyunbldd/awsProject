44165338048193df1f176c9b519126175b9e7827
Oyunbldd Ganbldd <oyunblddganbldd@Oyunbldds-iMac.local>
2022-02-24 18:26:00 +0800

    q

--- src/screens/loginScreen.tsx
+++ src/screens/loginScreen.tsx
@@ -1,10 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, {useState} from "react";
 import Button from "../components/button";
 import {TextInput} from "../components/textInput";
+import  AWS from 'aws-sdk/global';
+import { CognitoUser, AuthenticationDetails ,CognitoUserAttribute} from "amazon-cognito-identity-js";
+import userPool from "../components/userPool";
 import "../styles/main.css";
 export const Login = () => {
+  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const tilt=()=>{
+   let authenticationData={
+     Username: email,
+     Password: password,
+   }
+   let userData = {
+     Username:email,
+     Pool:userPool,
+   }
+   let authenticationDetails = new AuthenticationDetails(authenticationData);
+   let cognitoUser = new CognitoUser(userData);
+   cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: function(result) {
+      var accessToken = result.getAccessToken().getJwtToken();
+      console.log(accessToken);
+      AWS.config.region = '<region>';
+  
+      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: '...', // your identity pool id here
+        Logins: {
+          'cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>': result
+            .getIdToken()
+            .getJwtToken(),
+        },
+      });
+      console.log(AWS.config.credentials)
+    },
+  
+    onFailure: function(err) {
+      alert(err.message || JSON.stringify(err));
+    },
+  });
+  }
   return (
     <div id="container">
       <h1>Welcome to Login Screen</h1>
@@ -26,7 +64,7 @@ export const Login = () => {
       />
       <Button
         onClick={() => {
-          console.log("dqwdq");
+          tilt()
         }}
       >
         Sign in
--- src/screens/signUpScreen.tsx
+++ src/screens/signUpScreen.tsx
@@ -1,10 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, {useState} from "react";
 import Button from "../components/button";
 import {TextInput} from "../components/textInput";
+import { CognitoUser, AuthenticationDetails ,CognitoUserAttribute} from "amazon-cognito-identity-js";
+import userPool from "../components/userPool";
 import "../styles/main.css";
+var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 export const Signup = () => {
+  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const tilt=()=>{
+    var name={
+      Name:'name',
+      Value:'testing',
+    }
+    var phone={
+      Name:'phone_number',
+      Value:'+97699162182',
+    }
+    let attributeName=new CognitoUserAttribute(name);
+    let attributePhone=new CognitoUserAttribute(phone);
+    let a = []
+    a.push(attributeName)
+    a.push(attributePhone)
+  
+     userPool.signUp(email,password, a, [], (err,data)=>{ 
+        if(err) console.log(err);
+        console.log(data);
+     });
+  }
   return (
     <div id="container">
       <h1>Welcome to SignUp Screen</h1>
@@ -34,7 +59,7 @@ export const Signup = () => {
       />
       <Button
         onClick={() => {
-          console.log("dqwdq");
+          tilt();
         }}
       >
         Sign up
